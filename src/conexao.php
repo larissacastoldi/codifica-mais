903f1ba167d3fb6bbc57212058e80704cf48f9b7
larissacastoldi <larissaforesticastoldi94669@gmail.com>
2026-01-15 21:07:55 -0300

    Revert "configurando pc casa"
    This reverts commit 948b785765df91df27447ea0098854c838ac4a97.

--- src/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-$pdo = new PDO("mysql:host=localhost;dbname=sistema_login","root","L@ri2005");
-
-
-// Adicione esta linha abaixo da criação do objeto $pdo
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
